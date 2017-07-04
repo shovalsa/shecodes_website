91106c9a1c14eef821478d3414d3a873cbc20064
shovalsa <shovatz@gmail.com>
2017-07-04 14:29:02 +0300

    minor changes

--- sc_project/settings.py
+++ sc_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 's4dzp9iu-k-62x3u106z@=fvey@a)l$a5r)4u5dhwsi%^j%j$g'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['schowall.pythonanywhere.com']
 
 
 # Application definition
