5ea77a9257cfd8d9f5bfcb9f71038b85fa1bef8b
shovalsa <shovatz@gmail.com>
2017-08-29 19:47:12 +0300

    ALLOWED_HOSTS

--- sc_project/settings.py
+++ sc_project/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 's4dzp9iu-k-62x3u106z@=fvey@a)l$a5r)4u5dhwsi%^j%j$g'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', 'schowall.pythonanywhere.com']
 
 
 # Application definition
