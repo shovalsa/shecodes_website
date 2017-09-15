3fae1929184cdb905975c44c7928c1d0554cb238
shovalsa <shovatz@gmail.com>
2017-09-15 20:21:57 +0300

    fixing capture on index page carousel

--- scsite/static/bootstrap/js/shecodes.js
+++ scsite/static/bootstrap/js/shecodes.js
@@ -14,6 +14,7 @@ function carousel() {
       // Instantiate the Bootstrap carousel
   $('.multi-item-carousel').carousel({
     interval: false
+//    wrap: false
   });
 
   // for every slide in carousel, copy the next slide's item in the slide.
@@ -31,4 +32,36 @@ function carousel() {
       $(this).siblings(':first').children(':first-child').clone().appendTo($(this));
     }
   });
+}
+
+function sidebar(){
+    $(document).ready(function () {
+      var trigger = $('.hamburger'),
+          overlay = $('.overlay'),
+         isClosed = false;
+
+        trigger.click(function () {
+          hamburger_cross();
+        });
+
+        function hamburger_cross() {
+
+          if (isClosed == true) {
+            overlay.hide();
+            trigger.removeClass('is-open');
+            trigger.addClass('is-closed');
+            isClosed = false;
+          } else {
+            overlay.show();
+            trigger.removeClass('is-closed');
+            trigger.addClass('is-open');
+            isClosed = true;
+          }
+      }
+
+      $('[data-toggle="offcanvas"]').click(function () {
+            $('#wrapper').toggleClass('toggled');
+      });
+
+    });
 }
