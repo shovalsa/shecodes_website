f6914a7a7f608a57fcd03b5391ee4c53c79c8fb1
Inbal Mishory <mishory@gmail.com>
2017-09-27 23:35:26 +0300

    Fixed side-nav so overlay and hamburger icon display correctly. Removed depricated HTML tags, fixed some css rules and html readability.

--- scsite/static/bootstrap/js/shecodes.js
+++ scsite/static/bootstrap/js/shecodes.js
@@ -1,24 +1,33 @@
-function validate() {
-  var n1 = document.getElementById("num1");
-  var n2 = document.getElementById("num2");
-  if(n1.value != "" && n2.value != "") {
-    if(n1.value == n2.value) {
-      return true;
-    }
-  }
-  alert("The values should be equal and not blank");
-  return false;
-}
+$(document).ready(function () {
+  var trigger = $('.hamburger'),
+      overlay = $('.overlay'),
+      isClosed = false;
 
-function carousel() {
-      // Instantiate the Bootstrap carousel
+  $('[data-toggle="offcanvas"]').click(function () {
+    console.log("in data-toggle function");
+        $('#wrapper').toggleClass('toggled');
+        if (isClosed == true) {
+          overlay.hide();
+          trigger.removeClass('is-open');
+          trigger.addClass('is-closed');
+          isClosed = false;
+        } else {
+          overlay.show();
+          trigger.removeClass('is-closed');
+          trigger.addClass('is-open');
+          isClosed = true;
+        }
+  });
+  // Instantiate the Bootstrap carousel
   $('.multi-item-carousel').carousel({
-    interval: false
-//    wrap: false
+  interval: false
+  //    wrap: false
   });
 
-  // for every slide in carousel, copy the next slide's item in the slide.
-  // Do the same for the next, next item.
+  $('.multi-item-carousel').carousel({
+    interval: false
+  });
+
   $('.multi-item-carousel .item').each(function(){
     var next = $(this).next();
     if (!next.length) {
@@ -32,36 +41,18 @@ function carousel() {
       $(this).siblings(':first').children(':first-child').clone().appendTo($(this));
     }
   });
-}
-
-function sidebar(){
-    $(document).ready(function () {
-      var trigger = $('.hamburger'),
-          overlay = $('.overlay'),
-         isClosed = false;
-
-        trigger.click(function () {
-          hamburger_cross();
-        });
-
-        function hamburger_cross() {
-
-          if (isClosed == true) {
-            overlay.hide();
-            trigger.removeClass('is-open');
-            trigger.addClass('is-closed');
-            isClosed = false;
-          } else {
-            overlay.show();
-            trigger.removeClass('is-closed');
-            trigger.addClass('is-open');
-            isClosed = true;
-          }
+  function validate() {
+    var n1 = document.getElementById("num1");
+    var n2 = document.getElementById("num2");
+    if(n1.value != "" && n2.value != "") {
+      if(n1.value == n2.value) {
+        return true;
       }
+    }
+    alert("The values should be equal and not blank");
+    return false;
+  }
 
-      $('[data-toggle="offcanvas"]').click(function () {
-            $('#wrapper').toggleClass('toggled');
-      });
 
-    });
-}
+
+});
